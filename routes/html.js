4c87bbcf1ede4bc25a956ece3a500294bf7fdc8a
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-08-10 17:45:30 -0500

    updates to html.js file

---
+++ routes/html.js
@@ -0,0 +1,21 @@
+var router = require("express").Router();
+var path = require("path");
+const { Router } = require("express");
+
+router.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+});
+
+router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
+
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+module.exports = router;
