3e5c2ccd01bc4bb6460ccd63de4a42258d97d1c8
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-08-12 23:07:43 -0500

    Editing Server.js for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://nickforeman4:Whiting3543@ds127878.mlab.com:27878/heroku_v7kjz0vw";
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://nickforeman4:Whiting3543@ds127878.mlab.com:27878/heroku_v7kjz0vw";
 
-mongoose.connect(MONGODB_URI);
+//mongoose.connect(MONGODB_URI);
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -14,10 +14,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-//    useNewUrlParser: true,
-//    useFindAndModify: false
-//});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://nickforeman4:Whiting3543@ds127878.mlab.com:27878/heroku_v7kjz0vw", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
@@ -26,3 +26,5 @@ app.use(require("./routes/htmlRoutes.js"));
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
+
+//mongodb://localhost/workout
